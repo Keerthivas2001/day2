b0fa97e3e7adf8f0ed7a573e84053120dfbef062
Keerthivas2001 <keerthivasandhanasekar2001@gmail.com>
2022-09-21 14:52:53 +0530

    json for loop

---
+++ day3tasks/resume.js
@@ -0,0 +1,74 @@
+
+{
+    "basics" [
+        {
+      "name": "keerthivas.M.D",
+      "email": "keerthivasandhanasekar2001@gmail.com",
+      "phone": "8925616816",
+      "degree": "BBA SHIPPING",
+        }
+    ]
+      "location" [
+      {
+        "address": "No:34/57,periyar street, sanatorium tambaram",
+        "postalCode": "600047",
+        "city": "Chennai",
+        "country": "India",
+        "region": "Asia",
+      }
+    ]
+      "profiles"  [
+        {
+          "network": "LinkedIn",
+          "username": "Keerthi Vask",
+          "url": "www.linkedin.com/in/keerthi-vas-632775210",
+        }
+      ]
+    
+   
+    
+    "education" [
+    
+      {
+       " schoolName" : "N.S.N Group Of School",
+       "10thGpa" : "78",
+      " 10thPassedOutyear" : "2016",
+       "+2Gpa" : "62",
+       "+2PassedOutYear" : "2019",
+      }
+      
+      ]
+        
+         [
+      {
+        "institution": "AMET University",
+        "studyType": "SHIPPING AND LOGISTICS",
+        "startYear": "2019",
+        "endYear": "2022",
+        "gpa": "7",
+        "courses": "BBA SHIPPING",
+          
+        
+      }
+    ]
+       
+       
+    "skills" [
+      {
+        "name": "Microsoft(Excel,word and powerpoint)",
+        
+      }
+    ]
+    "languages" [
+      {
+        "language": "English and Tamil",
+        "fluency": "intermediate",
+      }
+    ]
+    "interests" [
+      {
+        "name": "Reading and writting stories",
+    
+      }
+    ]
+}
